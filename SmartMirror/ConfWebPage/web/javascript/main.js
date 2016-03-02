4f2de5ecf2c008bddadaf1479cfc1cdd809d6256
abhitha <abhitha65@gmail.com>
2016-03-02 18:19:28 -0500

    Adding Second Webpage Commit

---
+++ SmartMirror/ConfWebPage/web/javascript/main.js
@@ -0,0 +1,9 @@
+$( "form" ).on( "submit", function( event ) {
+  event.preventDefault();
+  console.log( $( this ).serialize() );
+});
+
+function submitFunc() {
+    console.log("I am here");
+    var test = $("#tbl :input").serialize();
+}
