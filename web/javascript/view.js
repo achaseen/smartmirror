b1e05c0adfa9093dd92c383a2f7593327797cf01
Anthony Chaseendran <achaseen@uwaterloo.ca>
2016-03-23 14:06:00 -0400

    upgraded our weather icons

--- web/javascript/view.js
+++ web/javascript/view.js
@@ -19,14 +19,34 @@ var ViewModule = (function() {
           var cloud = document.getElementById("clouds");
           var temp = document.getElementById("temp");
           var location = document.getElementById('location');
+          var weatherImage = document.getElementById('weatherImage')
           
           var promise = this.DataService.queryWeather();
           promise.then(function(result) {
-	     //debugger;
+            var prefix = 'wi wi-owm-';
+            var code = result.weather[0].id;
+            var iconD;
+            var dorn;
+
+            var today = new Date();
+            var hour = today.getHours();
+
+            if (hour > 6 && hour < 20) {
+                //Day time
+               dorn = "day-";
+
+            } else {
+                //Night time
+               dorn ="night-";
+            }
+
+             iconD = prefix +dorn+ code;
+
+             weatherImage.innerHTML = "<i class='" + iconD + "'></i>"
              location.innerHTML = result.name;
              temp.innerHTML = result.main.temp + " degrees";
              cloud.innerHTML = result.weather[0].description;
-             $("#image").attr("src", "http://openweathermap.org/img/w/" +result.weather[0].icon +".png");
+             // $("#image").attr("src", "http://openweathermap.org/img/w/" +result.weather[0].icon +".png");
           });
           
       },
